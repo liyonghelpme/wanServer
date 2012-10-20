ceea5aa9ebc09ec1137d75af454fb3d2126868f7
liyong <liyonghelpme@gmail.com>
2012-10-20 21:28:50 +0800

    参数表

---
+++ wangguo/model/gameParam.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Sample model module."""
+
+from sqlalchemy import *
+from sqlalchemy.orm import mapper, relation
+from sqlalchemy import Table, ForeignKey, Column
+from sqlalchemy.types import Integer, Unicode
+#from sqlalchemy.orm import relation, backref
+
+from wangguo.model import DeclarativeBase, metadata, DBSession
+
+
+class GameParam(object):
+    def __init__(self):
+        pass
