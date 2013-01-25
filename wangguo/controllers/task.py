bf55844a16befa6faf19706bbb46c8e78bd31bae
liyong <liyonghelpme@gmail.com>
2013-01-25 15:00:38 +0800

    删除每日任务 各种购买任务

--- wangguo/controllers/task.py
+++ wangguo/controllers/task.py
@@ -61,62 +61,6 @@ class TaskController(BaseController):
         return dict(id=1)
 
 
-
-    @expose('json')
-    def getBuyTask(self, uid):
-        uid = int(uid)
-        task = DBSession.query(UserBuyTaskRecord).filter_by(uid=uid).all()
-        tasks = [i.oid for i in task]
-        return dict(id=1, tasks=tasks)
-
-    
-    @expose('json')
-    def finishBuyTask(self, uid, oid, gain):
-        print gain
-        gain = json.loads(gain)
-        task = UserBuyTaskRecord(uid=uid, oid=oid)
-        DBSession.add(task)
-        doGain(uid, gain)
-        return dict(id=1)
-
-    #随机生成一批不同类型的每日任务
-    
-    #得到所有的每日任务
-    #分类每日任务
-    #产生3个类
-    #从3个类中 去除 3个任务
-    @expose('json')
-    def getDayTask(self, uid):
-        uid = int(uid)
-        allTasks = datas['allTasks'].items()
-        dayTask = {}#category ---> [a b c]
-        for i in allTasks:
-            if i[1]["kind"] == getParams("dayTask"):
-                cat = dayTask.setdefault(i[1]['dayCategory'], [])
-                cat.append(i[1])
-        
-        allCategory = dayTask.items()
-        res = []
-        #随机生成3个数
-        for i in xrange(0, 3):
-            if len(allCategory) > 0:
-                pos = random.randint(0, len(allCategory)-1)
-                res.append(allCategory[pos])
-                allCategory.pop(pos)
-        realTid = []
-        for i in res:
-            pos = random.randint(0, len(i[1])-1)
-            realTid.append(i[1][pos]['id'])
-        return dict(id=1, dayTid=realTid)
-    
-
-    #完成3个每日任务后产生奖励
-    @expose('json')
-    def finishDayTask(self, uid, gain):
-        uid = int(uid)
-        gain = json.loads(gain)
-        doGain(uid, gain)
-        return dict(id=1)
     @expose('json')
     def updateNewTaskStage(self, uid, newTaskStage):
         uid = int(uid)
